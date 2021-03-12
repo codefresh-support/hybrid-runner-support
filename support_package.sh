33ade6623211c3a924e5fe5202bc32f359ad42d0
Lukas Goodfellow <60825952+lukasgoodfellow@users.noreply.github.com>
2021-03-11 17:27:20 -0800

    Reorg script to get all pod information is the codefresh namespace

--- support_package.sh
+++ support_package.sh
@@ -9,45 +9,35 @@ NAMESPACE=$2
 # Set Date
 NOW=$(date '+%Y%m%d%H%M%S')
 
-
 echo "Creating Codefresh temp directory"
 mkdir -p codefresh/$NOW
-
-echo "Changing to temp directory"
 cd codefresh/$NOW
 
 echo "Exporting Nodes Descriptions"
-kubectl describe nodes >> kube-node-descriptions-$NOW.txt
+kubectl describe nodes >> node-describe.txt
 
-echo "Exporting Pod List"
-kubectl get pods -n $NAMESPACE -o wide >> kube-pod-list-$NOW.txt
-echo " " >> kube-pod-list-$NOW.txt
-kubectl get pods -n $NAMESPACE -o yaml >> kube-pod-list-$NOW.txt
+echo "Gather Pod information in the $NAMESPACE namespace"
+kubectl get pods -n $NAMESPACE -o wide >> pod-list.txt
 
-echo "Exporting Pod Descriptions"
-kubectl describe pods -n $NAMESPACE >> kube-pod-descriptions-$NOW.txt
+for POD in $(kubectl get pods -n $NAMESPACE --no-headers -o custom-columns=":metadata.name")
+do
+  mkdir $POD
+	kubectl get pods $POD -n $NAMESPACE -o yaml >> $POD/get.yaml
+  kubectl describe pods $POD -n $NAMESPACE >> $POD/describe.txt
+  kubectl logs $POD -n $NAMESPACE >> $POD/logs.log
+done
 
-echo "Exporting Pod Logs"
-mkdir pod-logs
-
-kubectl logs -n $NAMESPACE -l app=venona >> pod-logs/kube-venona-log-$NOW.log || echo "venona pod not found!"
-kubectl logs -n $NAMESPACE -l app=dind >> pod-logs/kube-dind-log-$NOW.log || echo "dind pod not found!"
-kubectl logs -n $NAMESPACE -l app=runtime >> pod-logs/kube-runtime-log-$NOW.log || echo "runtime pod not found!"
-kubectl logs -n $NAMESPACE -l app=dind-lv-monitor >> pod-logs/kube-dind-lv-monitor-log-$NOW.log || echo "dind lv monitor pod not found!"
-kubectl logs -n $NAMESPACE -l app=dind-volume-provisioner >> pod-logs/kube-dind-volume-provisioner-$NOW.log || echo "dind volume provisioner pod not found!"
-kubectl logs -n $NAMESPACE -l app=monitor >> pod-logs/kube-monitor-log-$NOW.log || echo "monitor pod not found!"
-kubectl logs -n $NAMESPACE -l app=runner >> pod-logs/kube-runner-log-$NOW.log || echo "runner pod not found!"
 
 echo "Exporting Codefresh Runtime"
-codefresh get runtime-environment $RUNTIME -o json >> cf-runtime-$NOW.json
+codefresh get runtime-environment $RUNTIME -o json >> cf-runtime.json
 
 echo "Exporting Deployments"
-kubectl get deployments -n $NAMESPACE -o yaml >> kube-cf-deployments-$NOW.yaml
+kubectl get deployments -n $NAMESPACE -o yaml >> cf-deployments.yaml
 
 echo "Exporting Storage Class"
-STORAGE_CLASS=$(jq .dockerDaemonScheduler.pvcs.dind.storageClassName cf-runtime-$NOW.json | sed 's/"//g')
+STORAGE_CLASS=$(jq .dockerDaemonScheduler.pvcs.dind.storageClassName cf-runtime.json | sed 's/"//g')
 
-kubectl get storageclass $STORAGE_CLASS -o yaml >> kube-cf-storageclass-$NOW.yaml
+kubectl get storageclass $STORAGE_CLASS -o yaml >> cf-storageclass.yaml
 
 echo "Archiving Contents and cleaning up"
 cd ../..
@@ -55,5 +45,4 @@ tar -czvf codefresh/codefresh-support-$NOW.tar.gz codefresh/$NOW
 rm -rf codefresh/$NOW
 
 echo "New Tar Package: codefresh/codefresh-support-$NOW.tar.gz"
-
 echo "Please attach .tar.gz to your support ticket"
