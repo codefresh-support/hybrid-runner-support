f8c8746bca253bf4ec7df05640a73da73340a598
Lukas Goodfellow <60825952+xplrior@users.noreply.github.com>
2021-10-12 12:31:12 -0700

    update readme and support_package

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 1. `kubectl config current-context` must be the context of the cluster the runner is located in
 2. [Codefresh CLI](https://codefresh-io.github.io/cli/installation/) must be installed and configured
 3. jq (used to gather information)
-3. git (can download a zip version instead)
+4. git (can download a zip version instead)
 
 ## Script Usage
 
@@ -16,29 +16,21 @@ This script is to gather information about the Codefresh Runner (previously Veno
 To begin, you will need to clone the repo, change the directory to this repo, and add execution flag to the script.  Below is the command to do it all in one go.
 
 ```bash
-git clone https://github.com/codefresh-contrib/venona-support.git && \
-cd venona-support && \
+git clone https://github.com/codefresh-support/hybrid-runner-support && \
+cd hybrid-runner-support && \
 chmod +x support_package.sh
 ```
 
 ### Syntax
 
 ```bash
-./support_package.sh "Codefresh/Runtime" AGENT-NAMESPACE
+./support_package.sh <Codefresh_Runtime>
 ```
 
-The 1st argument is the runtime name. You can run `codefresh get runtime-environments` to get the runtimes.
+The argument is the runtime name. You can run `codefresh get runtime-environments` to get the runtime names.
 
-> NOTE: make sure the runtime name is in `""` because of the `/` character.
-
-The 2nd is the Kubernetes Namespace where the Codefresh Agent is located. Only use if it is in a different namespace than the runtime environment. The AGENT-NAMESPACE argument is not required.
-
-Example:
+### Example
 
 ```bash
-./support_package.sh "sales-dev-eks/codefresh-runtime"
-```
-
-```bash
-./support_package.sh "sales-dev-eks/codefresh-runtime" codefresh-namespace
+./support_package.sh sales-dev-eks/codefresh-runtime
 ```
--- support_package.sh
+++ support_package.sh
@@ -1,175 +1,73 @@
 #!/usr/bin/env bash
 
-# Set Runtime Name
-RUNTIME=$1
-NAMESPACE2=$2
-
-# Set Date in tha year month day hour minutes seconds
+RENV=$1
 NOW=$(date '+%Y%m%d%H%M%S')
 
-# setting work directory
-echo "Creating Codefresh temp directory"
-mkdir -p codefresh/$NOW
-cd codefresh/$NOW
+echo "Creating Codefresh Temp Directory"
+mkdir -p codefresh-$NOW
+cd codefresh-$NOW
 
-# getting node information
-echo "Exporting Nodes Descriptions"
-kubectl describe nodes >> nodes.txt
+echo "Gathering Codefresh Runtime Information"
+codefresh get runtime-environment $RENV -o json > cf-runtime.json
+NAMESPACE=$(jq --raw-output '.dockerDaemonScheduler.cluster.namespace' cf-runtime.json)
+STORAGE_CLASS=$(jq --raw-output .dockerDaemonScheduler.pvcs.dind.storageClassName cf-runtime.json)
+kubectl describe nodes > nodes.txt
+kubectl get storageclass $STORAGE_CLASS -o yaml > storageClass.yaml
+kubectl get deployments -n $NAMESPACE -o yaml > deployments.yaml
+kubectl get daemonsets -n $NAMESPACE -o yaml > daemonsets.yaml
+kubectl get service -n $NAMESPACE -o yaml > services.yaml
+kubectl get events -n $NAMESPACE --sort-by=.metadata.creationTimestamp > events.txt
+kubectl get persistentvolume -l pod_namespace=$NAMESPACE -o wide > persistentVolume-list.txt
+kubectl get persistentvolumeclaim -n $NAMESPACE -o wide > persistentVolumeClaim-list.txt
+kubectl get pods -n $NAMESPACE -o wide > pod-list.txt
 
-# getting RE information. sed is there if more than 1 RE is atatched to an agent
-for RENV in $(codefresh get agents --sc runtimes | grep $RUNTIME | sed -e $'s/,/\\\n/g')
+echo "Gathering Hybrid Runner Information in $NAMESPACE namepspace"
+
+for PVC in $(kubectl get persistentvolumeclaim -n $NAMESPACE -l codefresh-app=dind --no-headers -o custom-columns=":metadata.name")
 do
-  NAMESPACE=$(codefresh get runtime-environments $RENV -o json | jq --raw-output '.dockerDaemonScheduler.cluster.namespace')
-  mkdir $NAMESPACE
-
-  echo "Exporting Codefresh Runtime"
-  codefresh get runtime-environment $RENV -o json >> $NAMESPACE/cf-runtime.json
-
-  echo "Exporting Storage Class"
-  STORAGE_CLASS=$(jq .dockerDaemonScheduler.pvcs.dind.storageClassName $NAMESPACE/cf-runtime.json | sed 's/"//g')
-  kubectl get storageclass $STORAGE_CLASS -o yaml >> $NAMESPACE/cf-storageclass.yaml
-
-  echo "Exporting Deployments in the $NAMESPACE namespace"
-  kubectl get deployments -n $NAMESPACE -o yaml >> $NAMESPACE/cf-deployments.yaml
-
-  echo "Gather Events for $NAMESPACE namespace"
-  kubectl get events -n $NAMESPACE --sort-by=.metadata.creationTimestamp >> $NAMESPACE/events.txt
-
-  echo "Gather PVC list for $NAMESPACE namespace"
-  kubectl get pvc -n $NAMESPACE -o wide >> $NAMESPACE/pvc-list.txt
-
-  echo "Gather PV list for $NAMESPACE namespace"
-  kubectl get pv -l pod_namespace=$NAMESPACE >> $NAMESPACE/pv-list.txt
-
-  echo "Gather Pod information in the $NAMESPACE namespace"
-  kubectl get pods -n $NAMESPACE -o wide >> $NAMESPACE/pod-list.txt
-
-  # For codefresh runner init installs
-  for POD in $(kubectl get pods -n $NAMESPACE -l 'app in (app-proxy, dind, dind-lv-monitor, dind-volume-provisioner, dind-volume-cleanup, runtime, runner, monitor, venona, volume-provisioner-monitor, volume-provisioner)' --no-headers -o custom-columns=":metadata.name")
-  do
-    mkdir $NAMESPACE/$POD
-    kubectl get pods $POD -n $NAMESPACE -o yaml >> $NAMESPACE/$POD/get.yaml
-    kubectl describe pods $POD -n $NAMESPACE >> $NAMESPACE/$POD/describe.txt
-    kubectl logs $POD -n $NAMESPACE --all-containers >> $NAMESPACE/$POD/logs.log
-  done
-
-  # For helm installs
-  for POD in $(kubectl get pods -n $NAMESPACE -l 'codefresh.io/application in (app-proxy, dind, dind-lv-monitor, dind-volume-provisioner, dind-volume-cleanup, runtime, runner, monitor, venona, volume-provisioner-monitor, volume-provisioner)' --no-headers -o custom-columns=":metadata.name")
-  do
-    mkdir $NAMESPACE/$POD
-    kubectl get pods $POD -n $NAMESPACE -o yaml >> $NAMESPACE/$POD/get.yaml
-    kubectl describe pods $POD -n $NAMESPACE >> $NAMESPACE/$POD/describe.txt
-    kubectl logs $POD -n $NAMESPACE --all-containers >> $NAMESPACE/$POD/logs.log
-  done
-
-  # Additional items from helm installs
-  for POD in $(kubectl get pods -n $NAMESPACE -l 'codefresh-app in (app-proxy, dind, dind-lv-monitor, dind-volume-provisioner, dind-volume-cleanup, runtime, runner, monitor, venona, volume-provisioner-monitor, volume-provisioner)' --no-headers -o custom-columns=":metadata.name")
-  do
-    mkdir $NAMESPACE/$POD
-    kubectl get pods $POD -n $NAMESPACE -o yaml >> $NAMESPACE/$POD/get.yaml
-    kubectl describe pods $POD -n $NAMESPACE >> $NAMESPACE/$POD/describe.txt
-    kubectl logs $POD -n $NAMESPACE --all-containers >> $NAMESPACE/$POD/logs.log
-  done
-
-  # getting PVC
-  echo "Gather PVC information in the $NAMESPACE namespace"
-  for PVC in $(kubectl get pvc -n $NAMESPACE -l 'codefresh-app=dind' --no-headers -o custom-columns=":metadata.name")
-  do
-    mkdir $NAMESPACE/$PVC
-    kubectl get pvc $PVC -n $NAMESPACE -o yaml >> $NAMESPACE/$PVC/get.yaml
-    kubectl describe pvc $PVC -n $NAMESPACE >> $NAMESPACE/$PVC/describe.txt
-  done
-
-  # getting PV
-  echo "Gather PV information in the $NAMESPACE namespace"
-  for PV in $(kubectl get pv -l 'codefresh-app=dind' -l pod_namespace=$NAMESPACE --no-headers -o custom-columns=":metadata.name")
-  do
-    mkdir $NAMESPACE/$PV
-    kubectl get pv $PV -o yaml >> $NAMESPACE/$PV/get.yaml
-    kubectl describe pv $PV >> $NAMESPACE/$PV/describe.txt
-  done
-
+  mkdir -p persistentVolumeClaim/$PVC
+  kubectl get persistentvolumeclaim $PVC -n $NAMESPACE -o yaml > persistentVolumeClaim/$PVC/get.yaml
+  kubectl describe persistentvolumeclaim $PVC -n $NAMESPACE > persistentVolumeClaim/$PVC/describe.txt
 done
 
-if [ -n "$NAMESPACE2" ]; then
+for PV in $(kubectl get persistentvolume -l codefresh-app=dind -l pod_namespace=$NAMESPACE --no-headers -o custom-columns=":metadata.name")
+do
+  mkdir -p persistentVolume/$PV
+  kubectl get persistentvolume $PV -o yaml > persistentVolume/$PV/get.yaml
+  kubectl describe persistentvolume $PV > persistentVolume/$PV/describe.txt
+done
 
-  mkdir $NAMESPACE2
+# For codefresh runner init installs
+for POD in $(kubectl get pods -n $NAMESPACE -l 'app in (app-proxy, dind, dind-lv-monitor, dind-volume-provisioner, dind-volume-cleanup, runtime, runner, monitor, venona, volume-provisioner-monitor, volume-provisioner)' --no-headers -o custom-columns=":metadata.name")
+do
+  mkdir -p pods/$POD
+  kubectl get pods $POD -n $NAMESPACE -o yaml >> pods/$POD/get.yaml
+  kubectl describe pods $POD -n $NAMESPACE >> pods/$POD/describe.txt
+  kubectl logs $POD -n $NAMESPACE --all-containers >> pods/$POD/logs.log
+done
 
-  echo "Exporting Deployments in the $NAMESPACE2 namespace"
-  kubectl get deployments -n $NAMESPACE2 -o yaml >> $NAMESPACE2/cf-deployments-$NAMESPACE2.yaml
+# For helm installs
+for POD in $(kubectl get pods -n $NAMESPACE -l 'codefresh.io/application in (app-proxy, dind, dind-lv-monitor, dind-volume-provisioner, dind-volume-cleanup, runtime, runner, monitor, venona, volume-provisioner-monitor, volume-provisioner)' --no-headers -o custom-columns=":metadata.name")
+do
+  mkdir -p pods/$POD
+  kubectl get pods $POD -n $NAMESPACE -o yaml >> pods/$POD/get.yaml
+  kubectl describe pods $POD -n $NAMESPACE >> pods/$POD/describe.txt
+  kubectl logs $POD -n $NAMESPACE --all-containers >> pods/$POD/logs.log
+done
 
-  echo "Exporting Storage Class"
-  STORAGE_CLASS=$(jq .dockerDaemonScheduler.pvcs.dind.storageClassName $NAMESPACE2/cf-runtime.json | sed 's/"//g')
-  kubectl get storageclass $STORAGE_CLASS -o yaml >> $NAMESPACE2/cf-storageclass.yaml
+# Additional items from helm installs
+for POD in $(kubectl get pods -n $NAMESPACE -l 'codefresh-app in (app-proxy, dind, dind-lv-monitor, dind-volume-provisioner, dind-volume-cleanup, runtime, runner, monitor, venona, volume-provisioner-monitor, volume-provisioner)' --no-headers -o custom-columns=":metadata.name")
+do
+  mkdir -p pods/$POD
+  kubectl get pods $POD -n $NAMESPACE -o yaml >> pods/$POD/get.yaml
+  kubectl describe pods $POD -n $NAMESPACE >> pods/$POD/describe.txt
+  kubectl logs $POD -n $NAMESPACE --all-containers >> pods/$POD/logs.log
+done
 
-  echo "Gather Pod information in the $NAMESPACE2 namespace"
-  kubectl get pods -n $NAMESPACE2 -o wide >> $NAMESPACE2/pod-list.txt
-
-  echo "Gather Events for $NAMESPACE2 namespace"
-  kubectl get events -n $NAMESPACE2 --sort-by=.metadata.creationTimestamp >> $NAMESPACE2/events.txt
-
-  echo "Gather PVC list for $NAMESPACE2 namespace"
-  kubectl get pvc -n $NAMESPACE2 -o wide >> $NAMESPACE2/pvc-list.txt
-
-  echo "Gather PV list for $NAMESPACE2 namespace"
-  kubectl get pv -l pod_namespace=$NAMESPACE2 >> $NAMESPACE2/pv-list.txt
-
-  echo "Gather Pod information in the $NAMESPACE2 namespace"
-  kubectl get pods -n $NAMESPACE2 -o wide >> $NAMESPACE2/pod-list.txt
-
-  # For codefresh runner init installs
-  for POD in $(kubectl get pods -n $NAMESPACE2 -l 'app in (app-proxy, dind, dind-lv-monitor, dind-volume-provisioner, dind-volume-cleanup, runtime, runner, monitor, venona, volume-provisioner-monitor, volume-provisioner)' --no-headers -o custom-columns=":metadata.name")
-  do
-    mkdir $NAMESPACE2/$POD
-    kubectl get pods $POD -n $NAMESPACE2 -o yaml >> $NAMESPACE2/$POD/get.yaml
-    kubectl describe pods $POD -n $NAMESPACE2 >> $NAMESPACE2/$POD/describe.txt
-    kubectl logs $POD -n $NAMESPACE2 --all-containers >> $NAMESPACE2/$POD/logs.log
-  done
-
-  # For helm installs
-  for POD in $(kubectl get pods -n $NAMESPACE2 -l 'codefresh.io/application in (app-proxy, dind, dind-lv-monitor, dind-volume-provisioner, dind-volume-cleanup, runtime, runner, monitor, venona, volume-provisioner-monitor, volume-provisioner)' --no-headers -o custom-columns=":metadata.name")
-  do
-    mkdir $NAMESPACE2/$POD
-    kubectl get pods $POD -n $NAMESPACE2 -o yaml >> $NAMESPACE2/$POD/get.yaml
-    kubectl describe pods $POD -n $NAMESPACE2 >> $NAMESPACE2/$POD/describe.txt
-    kubectl logs $POD -n $NAMESPACE2 --all-containers >> $NAMESPACE2/$POD/logs.log
-  done
-
-  # Additional items from helm installs
-  for POD in $(kubectl get pods -n $NAMESPACE2 -l 'codefresh-app in (app-proxy, dind, dind-lv-monitor, dind-volume-provisioner, dind-volume-cleanup, runtime, runner, monitor, venona, volume-provisioner-monitor, volume-provisioner)' --no-headers -o custom-columns=":metadata.name")
-  do
-    mkdir $NAMESPACE2/$POD
-    kubectl get pods $POD -n $NAMESPACE2 -o yaml >> $NAMESPACE2/$POD/get.yaml
-    kubectl describe pods $POD -n $NAMESPACE2 >> $NAMESPACE2/$POD/describe.txt
-    kubectl logs $POD -n $NAMESPACE2 --all-containers >> $NAMESPACE2/$POD/logs.log
-  done
-
-  runtime_env=$(echo $RUNTIME | sed -e 's/\//-/g') #get runtime label
-
-  echo "Gather PVC information in the $NAMESPACE2 namespace"
-
-  for PVC in $(kubectl get pvc -n $NAMESPACE2 -l 'codefresh-app=dind' --no-headers -o custom-columns=":metadata.name")
-  do
-    mkdir $NAMESPACE2/$PVC
-    kubectl get pvc $PVC -n $NAMESPACE2 -o yaml >> $NAMESPACE2/$PVC/get.yaml
-    kubectl describe pvc $PVC -n $NAMESPACE2 >> $NAMESPACE2/$PVC/describe.txt
-  done
-
-  echo "Gather PV information in the $NAMESPACE2 namespace"
-  for PV in $(kubectl get pv -l 'codefresh-app=dind' -l pod_namespace=$NAMESPACE2 --no-headers -o custom-columns=":metadata.name")
-  do
-    mkdir $NAMESPACE2/$PV
-    kubectl get pv $PV -o yaml >> $NAMESPACE2/$PV/get.yaml
-    kubectl describe pv $PV >> $NAMESPACE2/$PV/describe.txt
-  done
-
-fi
-
-# compressing and cleaning up
 echo "Archiving Contents and cleaning up"
-cd ../..
-tar -czvf codefresh/codefresh-support-$NOW.tar.gz codefresh/$NOW
-rm -rf codefresh/$NOW
+cd ..
+tar -czf codefresh-support-$NOW.tar.gz codefresh-$NOW
+rm -rf codefresh-$NOW
 
-echo "New Tar Package: codefresh/codefresh-support-$NOW.tar.gz"
-echo "Please attach .tar.gz to your support ticket"
+echo "New Tar Package: codefresh-support-$NOW.tar.gz"
+echo "Please attach codefresh-support-$NOW.tar.gz to your support ticket"
